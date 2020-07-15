0c64ad2ba1552740caaad171462f393fe7f4fefd
Ryan Beiden <rdbeiden@gmail.com>
2020-07-14 22:49:48 -0400

    wip / add navbar with styling just needs specific ids added to each button for future reference

--- src/javascripts/components/navbar/navbar.js
+++ src/javascripts/components/navbar/navbar.js
@@ -1,28 +1,35 @@
 import utils from '../../helpers/utils';
+import './navbar.scss';
 
 const buildNavbar = () => {
   const domString = `
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Navbar</a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#"><span style="margin-right: 0px;">Le Baguette. <i class="fas fa-bread-slice"></i> </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#"><i class="fas fa-thermometer"></i></a>
-          </li>
-        </ul>
+      <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
+        <div class="nav-group">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Menu</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Staff</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Ingredients</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Reservations</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Seating</a>
+            </li>
+          </ul>
+        </div>
       </div>
+      <button class="btn btn-primary">Sign In</button>
     </nav>
   `;
 
