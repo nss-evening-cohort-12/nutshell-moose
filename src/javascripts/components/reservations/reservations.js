5f55b5f56d13e2b22735e401f66b0486a64b615c
Pete Stewart <petestew@gmail.com>
2020-07-15 20:51:39 -0500

    wip

--- src/javascripts/components/reservations/reservations.js
+++ src/javascripts/components/reservations/reservations.js
@@ -1,28 +1,35 @@
 // import authData from '../../helpers/data/authData';
+import moment from 'moment';
 import utils from '../../helpers/utils';
 import './reservations.scss';
 
+// const now = moment(Date.now()).format('MMMM Do YYYY, h:mm:ss a');
+
 const reservationForm = () => {
+  const today = moment(Date.now()).format('YYYY-MM-DD');
   const form = `
   <div id="reservation-form" class="mt-5 mx-auto">
     <form>
       <div class="form-group row">
         <label for="name" class="col-sm-1 col-form-label">Name:</label>
         <div class="col-sm-5">
-          <input type="text" class="form-control" id="name">
+          <input type="text" class="form-control" id="name" required>
         </div>
-        <label for="Date" class="col-sm-1 col-form-label">Date:</label>
+        <label for="date" class="col-sm-1 col-form-label">Date:</label>
         <div class="col-sm-5">
-          <input type="text" class="form-control" id="date">
+          <input type="date" min="${today}" class="form-control" id="date" required>
         </div>
       </div>
       <div class="form-group row">
         <label for="size" class="col-sm-1 col-form-label">Party size:</label>
         <div class="col-sm-1">
-          <input type="number" class="form-control" id="size">
+          <input type="number" class="form-control" id="size required">
         </div>
         <div class="col-sm-4"></div>
-        <div class="col-sm-6"></div>
+        <label for="time" class="col-sm-1 col-form-label">Time:</label>
+        <div class="col-sm-5">
+          <input type="time" min="11:00" max="21:00" class="form-control" id="time" required>
+        </div>
       </div>
       <div class="form-group row">
         <div class="col-sm-8"></div> 
