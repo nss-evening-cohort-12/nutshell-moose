13c6ef4182b0468c63efd0be1b1597253edee712
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-07-16 03:46:35 -0500

    wip

--- src/javascripts/components/menu/menu.js
+++ src/javascripts/components/menu/menu.js
@@ -7,10 +7,33 @@ const menuDom = (data) => {
   data.forEach((menuItem) => {
     domString += `
             <div class="card">
-            <img src="${menuItem.imgUrl}" class="card-img-top">
-            <div class="card-body">
-              <h5 class="card-title">${menuItem.name}</h5>
-              <p class="card-text">$${menuItem.price.toFixed(2)}</p>
+            <div class="card-img-top flip-container">
+              <div class="flipper">
+                <div class="front">
+                  <img src="${menuItem.imgUrl}" class="card-img-top">
+                  <div class="flip-arrow"><i class="fas fa-reply fa-lg"></i></div>
+                </div>
+                <div class="back">
+                  <ul>
+                    <li>Ingredient 1</li>
+                    <li>Ingredient 2</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="card-body d-flex flex-column h-100">
+              <h5 class="card-title mt-auto">${menuItem.name}</h5>
+              <p class="card-text mt-auto">$${menuItem.price.toFixed(2)}</p>
+              <div class="d-flex justify-content-end flex-nowrap mt-auto">
+              <span class="fa-stack fa-lg">
+                <i class="fa fa-circle fa-stack-2x"></i>
+                <i class="fa fa-pen fa-stack-1x fa-inverse"></i>
+              </span>
+              <span class="fa-stack fa-lg">
+                <i class="fa fa-circle fa-stack-2x"></i>
+                <i class="fa fa-trash fa-stack-1x fa-inverse"></i>
+              </span>
+              </div>
             </div>
           </div>
             `;
--- src/javascripts/helpers/clickEvents.js
+++ src/javascripts/helpers/clickEvents.js
@@ -2,6 +2,9 @@ import menu from '../components/menu/menu';
 
 const clickEvents = () => {
   $('body').on('click', '#menu-link', menu.menuItems);
+  $('body').on('click', '.flip-container', (e) => {
+    $(e.currentTarget).toggleClass('flipped');
+  });
 };
 
 export default { clickEvents };
