62065ffc1f08e9ac6f2ac2b504c617bd28d8d0bf
Pete Stewart <petestew@gmail.com>
2020-07-16 09:19:25 -0500

    form and existing reservations displaying

--- src/javascripts/helpers/clickEvents.js
+++ src/javascripts/helpers/clickEvents.js
@@ -1,17 +1,9 @@
 import displayStaff from '../components/displayStaff/displayStaff';
 import reservations from '../components/reservations/reservations';
 
-const createRes = (e) => {
-  e.preventDefault();
-  console.error(e);
-  const date = $('#date').val();
-  console.error(date);
-};
-
 const clickEvents = () => {
   $('body').on('click', '#staff-link', displayStaff.buildStaffConsole);
   $('body').on('click', '#reservations-link', reservations.reservationsPage);
-  $('body').on('click', '#create-reservation', createRes);
 };
 
 export default { clickEvents };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,7 +2,6 @@ import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
 import authData from './helpers/data/authData';
 import clickEvents from './helpers/clickEvents';
-import reservations from './components/reservations/reservations';
 import navbar from './components/navbar/navbar';
 import 'bootstrap';
 import '../styles/main.scss';
@@ -11,7 +10,6 @@ const init = () => {
   firebase.initializeApp(apiKeys.firebaseConfig);
   authData.checkLoginStatus();
   clickEvents.clickEvents();
-  reservations.reservationsPage();
   navbar.buildNavbar();
 };
 
