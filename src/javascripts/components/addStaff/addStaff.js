dd3df992c7904180bf5aa256cf1374ab5001eaa7
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2020-07-17 10:30:09 -0500

    WIP

--- src/javascripts/components/addStaff/addStaff.js
+++ src/javascripts/components/addStaff/addStaff.js
@@ -9,32 +9,32 @@ const addStaffDropDown = () => {
     </button>
     <div class="dropdown-menu">
       <form class="px-4 py-3">
-      <div class="form-group">
-        <label for="name">Name</label>
-        <input type="test" class="form-control" id="name" placeholder="Name">
-      </div>
-      <div class="form-group">
-      <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Filter by Staff
-      </button>
-      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <button class="dropdown-item" id="Busser" type="button">Busser</button>
-        <button class="dropdown-item" id="Server" type="button">Server</button>
-        <button class="dropdown-item" id="Chef" type="button">Chef</button>
-        <button class="dropdown-item" id="Manager" type="button">Manager</button>
-        <button class="dropdown-item" id="Host" type="button">Host</button>        
-      </div>
-      </div>
-      </div>
-      <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="dropdownCheck">
-        <label class="form-check-label" for="dropdownCheck">
-          Remember me
-        </label>
-      </div>
-      <button type="submit" class="btn btn-primary">Sign in</button>
-    </form>
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input type="text" class="form-control" id="name" placeholder="Name">
+        </div>
+        <div class="form-group">
+          <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Filter by Staff
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <button class="dropdown-item" id="Busser" type="button">Busser</button>
+              <button class="dropdown-item" id="Server" type="button">Server</button>
+              <button class="dropdown-item" id="Chef" type="button">Chef</button>
+              <button class="dropdown-item" id="Manager" type="button">Manager</button>
+              <button class="dropdown-item" id="Host" type="button">Host</button>        
+            </div>
+          </div>
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="dropdownCheck">
+          <label class="form-check-label" for="dropdownCheck">
+            Remember me
+          </label>
+        </div>
+        <button type="submit" class="btn btn-primary">Add New Staff</button>
+      </form>
 
     </div>
   </div>
