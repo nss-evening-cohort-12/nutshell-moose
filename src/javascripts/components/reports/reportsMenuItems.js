675357c0ed7aeff28b8b659099ecb6440c33ce0d
Geno McNew <genemcnew3@gmail.com>
2020-07-28 20:08:43 -0500

    displays popular and least popular menu items

--- src/javascripts/components/reports/reportsMenuItems.js
+++ src/javascripts/components/reports/reportsMenuItems.js
@@ -32,6 +32,54 @@ const getOrderedMenuItems = () => new Promise((resolve, reject) => {
     .catch((err) => reject(err));
 });
 
+const compareLow = (a, b) => {
+  if (a.count < b.count) {
+    return -1;
+  }
+  if (a.count > b.count) {
+    return 1;
+  }
+  return 0;
+};
+
+const compareHigh = (a, b) => {
+  if (a.count > b.count) {
+    return -1;
+  }
+  if (a.count < b.count) {
+    return 1;
+  }
+  return 0;
+};
+
+const drawCards = (e) => {
+  const buttonClicked = e.target.id;
+  getOrderedMenuItems().then((data) => {
+    if (buttonClicked === 'mostPopular') {
+      data.sort(compareHigh);
+    } else {
+      data.sort(compareLow);
+    }
+    let domString = '<div id="menuItemCards" class="justify-content-center d-flex flex-wrap">';
+    data.forEach((d) => {
+      domString += `
+      <div class="p-2">
+        <div class="card" style="width: 18rem;">
+        <img src="${d.imgUrl}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${d.name}</h5>
+          <p class="card-text">$${d.price}</p>
+          <p class="card-text">Sold: ${d.count}</p>
+        </div>
+      </div>
+    </div>`;
+    });
+    domString += '</div>';
+    console.warn(domString);
+    utils.printToDom('div #reportsDisplay', domString);
+  });
+};
+
 const drawMenuItems = () => {
   const domString = `
     <div class="d-flex flex-row justify-content-center">
@@ -39,12 +87,14 @@ const drawMenuItems = () => {
       <input type="radio" name="options" id="mostPopular" checked> Most Popular
       </label>
       <label class="m-3 btn btn-secondary">
-      <input type="radio" name="options" id="LeastPopular"> Least Popular
+      <input type="radio" name="options" id="leastPopular"> Least Popular
       </label>
     </div>
     <div id="reportsDisplay">cards go here</div>`;
   utils.printToDom('#menuItemsDiv', domString);
-  getOrderedMenuItems();
+  $('#mostPopular').click(drawCards);
+  $('#leastPopular').click(drawCards);
+  $('#mostPopular').click();
 };
 
 export default { drawMenuItems };
