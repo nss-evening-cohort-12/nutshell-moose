aa1ed6c962acc720aeefe9c823c666d7dd5d150d
Nikhil Gaikwad <gaikwadrnikhil@gmail.com>
2020-08-01 18:18:25 -0500

    finished the ingredients report functionality

--- src/javascripts/components/reports/reportsIngredients.js
+++ src/javascripts/components/reports/reportsIngredients.js
@@ -8,36 +8,36 @@ const drawIngredients = () => {
     <div class="row d-flex justify-content-center">
     <div class="col-6 d-flex flex-column">
       <label class="btn btn-secondary active">
-      <input type="radio" name="options" id="1Day" checked> 1 Day
+      <input type="radio" name="options" id="1IngredDay" value="1IngredDay" checked> 1 Day
       </label>
       <label class="btn btn-secondary">
-      <input type="radio" name="options" id="7Day" value="7Day"> Date Range
+      <input type="radio" name="options" id="7IngredDay" value="7IngredDay"> Date Range
       </label>
       <label class="btn btn-secondary">
-        <input type="radio" name="options" id="allDays" value="allDays"> All Days
+        <input type="radio" name="options" id="allIngredDays" value="allIngredDays"> All Days
       </label>
     </div>
     <div id="dateContainer" class="col-6 d-flex flex-row inline-block">
     <div class="m-1">
     <label for="1date">1 Day/Start Date</label>
-    <input type="date" class="m-1 form-control" id="1date" value="${today}">
+    <input type="date" class="m-1 form-control" id="1IngredDate" value="${today}">
     </div>
     <div class="m-1">
-    <label for="1date">End Date</label>
-    <input type="date" class="m-1 form-control" id="7date" value="${today}">
+    <label for="7date">End Date</label>
+    <input type="date" class="m-1 form-control" id="7IngredDate" value="${today}">
     </div>
     </div>
     <div>
     <button id="ingredientsSubmit" class="m5 btn btn-secondary submit">Submit</button>
     </div>
     </div>
-    <div id="reportsDisplay">cards go here</div>`;
+    <div id="reportsIngredDisplay">cards go here</div>`;
   utils.printToDom('#ingredientsDiv', domString);
 };
 
 const get7DayIngredients = () => {
-  const pickDate1 = $('#1date').val();
-  const pickDate2 = $('#7date').val();
+  const pickDate1 = $('#1IngredDate').val();
+  const pickDate2 = $('#7IngredDate').val();
   // get reservations in the range of these dates
   let domString = '';
   ingredientsSmash.get7DayIngredAmount(pickDate2, pickDate1)
@@ -45,7 +45,7 @@ const get7DayIngredients = () => {
       domString += `
         <div class="mt-3 card text-center">
           <div class="card-header">
-            All Revenue
+            Ingredients Used
           </div>
           <div class="card-body">
             <h2 class="card-title">Amount of Ingredients Used</h2>`;
@@ -60,7 +60,64 @@ const get7DayIngredients = () => {
           </div>
         </div>
       `;
-      utils.printToDom('div #reportsDisplay', domString);
+      utils.printToDom('div #reportsIngredDisplay', domString);
+    })
+    .catch((err) => console.warn('did not bring the reservation ', err));
+};
+
+const getOneDayIngredients = () => {
+  const pickDate1 = $('#1IngredDate').val();
+  // get reservations in the range of these dates
+  let domString = '';
+  ingredientsSmash.getOneDayIngredAmount(pickDate1)
+    .then((rawIngredients) => {
+      domString += `
+        <div class="mt-3 card text-center">
+          <div class="card-header">
+            Ingredients Used
+          </div>
+          <div class="card-body">
+            <h2 class="card-title">Amount of Ingredients Used</h2>`;
+      const keys = Object.keys(rawIngredients);
+      keys.forEach((key) => {
+        domString += `
+            <h3 class="card-text"> ${key}: ${rawIngredients[key]}</h3>`;
+      });
+      domString += `
+          </div>
+          <div class="card-footer text-muted">
+          </div>
+        </div>
+      `;
+      utils.printToDom('div #reportsIngredDisplay', domString);
+    })
+    .catch((err) => console.warn('did not bring the reservation ', err));
+};
+
+const getAllDayIngredients = () => {
+  // get reservations in the range of these dates
+  let domString = '';
+  ingredientsSmash.getAllDayIngredAmount()
+    .then((rawIngredients) => {
+      domString += `
+        <div class="mt-3 card text-center">
+          <div class="card-header">
+            Ingredients Used
+          </div>
+          <div class="card-body">
+            <h2 class="card-title">Amount of Ingredients Used</h2>`;
+      const keys = Object.keys(rawIngredients);
+      keys.forEach((key) => {
+        domString += `
+            <h3 class="card-text"> ${key}: ${rawIngredients[key]}</h3>`;
+      });
+      domString += `
+          </div>
+          <div class="card-footer text-muted">
+          </div>
+        </div>
+      `;
+      utils.printToDom('div #reportsIngredDisplay', domString);
     })
     .catch((err) => console.warn('did not bring the reservation ', err));
 };
@@ -70,29 +127,29 @@ const pickReport = () => {
   let pickDate1;
   let pickDate2;
   switch (radioVal) {
-    case '1Day':
+    case '1IngredDay':
       // console.warn('this is one day report', valid);
-      pickDate1 = $('#1date').val();
+      pickDate1 = $('#1IngredDate').val();
       if (pickDate1) {
         // eslint-disable-next-line no-use-before-define
-        // getOneDayRevenue();
+        getOneDayIngredients();
         // console.warn('there is date selected', pickDate1, valid);
       } else {
-        utils.printToDom('#reportsDisplay', 'Please select the date to get the report for it!!');
+        utils.printToDom('div #reportsIngredDisplay', 'Please select the date to get the report for it!!');
       }
       break;
-    case '7Day':
+    case '7IngredDay':
       // console.warn('this is 7 days report', valid);
-      pickDate2 = $('#7date').val();
-      pickDate1 = $('#1date').val();
+      pickDate2 = $('#7IngredDate').val();
+      pickDate1 = $('#1IngredDate').val();
       if (pickDate1 && pickDate2) {
         get7DayIngredients();
         // console.warn('there is date selected', pickDate1, pickDate2, valid);
       }
       break;
-    case 'allDays':
+    case 'allIngredDays':
       // console.warn('this is all days report');
-      // getAllRevenue();
+      getAllDayIngredients();
       break;
     default:
       // do nothing
--- src/javascripts/helpers/data/ingredientsSmash.js
+++ src/javascripts/helpers/data/ingredientsSmash.js
@@ -4,11 +4,110 @@ import orderData from './orderData';
 import menuIngrediantData from './menuIngrediantData';
 import ingredData from './ingredData';
 
+const getIngredsByfilteredOrder = (filteredOrdersList) => new Promise((resolve, reject) => {
+  // Get all menungredients
+  menuIngrediantData.getAllMenuIngredients()
+    .then((menuIngredients) => {
+      const menuIngredientsList = utils.firebaseArray(menuIngredients.data);
+      // console.warn(menuIngredientsList);
+      const selectedIngredientList = [];
+      // filter only menuingredients that were ordered by menuId
+      filteredOrdersList.forEach((menuItem) => {
+        const filteredMeuIngredientList = menuIngredientsList.filter((m) => m.menuId === menuItem.menuId);
+        selectedIngredientList.push(...filteredMeuIngredientList);
+      });
+      // console.warn(selectedIngredientList);
+      // just get the clean list of ingredients used in those orders or menu items
+      // ["ingredient9", "ingredient2", "ingredient7", "ingredient1", ....]
+      const cleanIngredientsList = selectedIngredientList.map((x) => x.ingredientId);
+      // console.warn(cleanIngredientsList);
+
+      // get the distinct values of ingredient and their count
+      // { ingredient7: 25, ingredient4: 15, ingredient2: 10, ingredient1: 5, ingredient8: 5, ...}
+      const uniqueValueAndCount = {};
+      cleanIngredientsList.forEach((ingredient) => {
+        if (ingredient in uniqueValueAndCount) {
+          uniqueValueAndCount[ingredient] += 1;
+        } else {
+          uniqueValueAndCount[ingredient] = 1;
+        }
+      });
+
+      // console.warn(uniqueValueAndCount);
+      const ingredientsUsed = Object.keys(uniqueValueAndCount);
+      const uniqueNameAndCount = {};
+      // get all the raw ingredients
+      ingredData.getIngredients()
+        .then((ingredients) => {
+          // filter the raw ingredients based on the ingredients used in the orders
+          ingredientsUsed.forEach((ingredient) => {
+            const filteredIngredients = ingredients.filter((m) => m.id === ingredient);
+            // insert the items in the uniqueNameAndCount object
+            uniqueNameAndCount[filteredIngredients[0].name] = uniqueValueAndCount[ingredient];
+          });
+          // console.warn(uniqueNameAndCount);
+          // prints {Beef: 25, Potato: 15, Spinach: 10, Rice: 5, Chicken: 5, ...}
+          resolve(uniqueNameAndCount);
+        });
+    })
+    .catch((err) => reject(err));
+});
+
 const get7DayIngredAmount = (pickDate2, pickDate1) => new Promise((resolve, reject) => {
   reservationsData.getReservationsByDateRange(pickDate2, pickDate1)
     .then((response) => {
       const reservationsList = utils.firebaseArray(response.data);
       // get all the orders
+      // console.warn(reservationsList);
+      orderData.getAllOrders()
+        .then((orders) => {
+          const ordersList = utils.firebaseArray(orders.data);
+          // console.warn(ordersList);
+          const filterdOrdersList = [];
+          // filter orders list by reservations in the date ranges
+          reservationsList.forEach((reservations) => {
+            const filteredList = ordersList.filter((p) => p.reservationsId === reservations.id);
+            filterdOrdersList.push(...filteredList);
+          });
+          getIngredsByfilteredOrder(filterdOrdersList)
+            .then((uniqueNameAndCount) => {
+              resolve(uniqueNameAndCount);
+            });
+        });
+    })
+    .catch((err) => reject(err));
+});
+
+const getOneDayIngredAmount = (pickDate1) => new Promise((resolve, reject) => {
+  reservationsData.getReservations(pickDate1)
+    .then((reservationsList) => {
+      // get all the orders
+      // console.warn(reservationsList);
+      orderData.getAllOrders()
+        .then((orders) => {
+          const ordersList = utils.firebaseArray(orders.data);
+          // console.warn(ordersList);
+          const filterdOrdersList = [];
+          // filter orders list by reservations in the date ranges
+          reservationsList.forEach((reservations) => {
+            const filteredList = ordersList.filter((p) => p.reservationsId === reservations.id);
+            filterdOrdersList.push(...filteredList);
+          });
+          getIngredsByfilteredOrder(filterdOrdersList)
+            .then((uniqueNameAndCount) => {
+              resolve(uniqueNameAndCount);
+            });
+        });
+    })
+    .catch((err) => reject(err));
+});
+
+const getAllDayIngredAmount = () => new Promise((resolve, reject) => {
+  reservationsData.getAllReservations()
+    .then((allReservations) => {
+      const reservationsList = utils.firebaseArray(allReservations.data);
+      // get all the orders
+      console.warn(reservationsList);
       orderData.getAllOrders()
         .then((orders) => {
           const ordersList = utils.firebaseArray(orders.data);
@@ -19,56 +118,13 @@ const get7DayIngredAmount = (pickDate2, pickDate1) => new Promise((resolve, reje
             const filteredList = ordersList.filter((p) => p.reservationsId === reservations.id);
             filterdOrdersList.push(...filteredList);
           });
-          console.warn(filterdOrdersList);
-          // Get all menungredients
-          menuIngrediantData.getAllMenuIngredients()
-            .then((menuIngredients) => {
-              const menuIngredientsList = utils.firebaseArray(menuIngredients.data);
-              console.warn(menuIngredientsList);
-              const selectedIngredientList = [];
-              // filter only menuingredients that were ordered by menuId
-              filterdOrdersList.forEach((menuItem) => {
-                const filteredMeuIngredientList = menuIngredientsList.filter((m) => m.menuId === menuItem.menuId);
-                selectedIngredientList.push(...filteredMeuIngredientList);
-              });
-              console.warn(selectedIngredientList);
-              // just get the clean list of ingredients used in those orders or menu items
-              // ["ingredient9", "ingredient2", "ingredient7", "ingredient1", ....]
-              const cleanIngredientsList = selectedIngredientList.map((x) => x.ingredientId);
-              console.warn(cleanIngredientsList);
-
-              // get the distinct values of ingredient and their count
-              // { ingredient7: 25, ingredient4: 15, ingredient2: 10, ingredient1: 5, ingredient8: 5, ...}
-              const uniqueValueAndCount = {};
-              cleanIngredientsList.forEach((ingredient) => {
-                if (ingredient in uniqueValueAndCount) {
-                  uniqueValueAndCount[ingredient] += 1;
-                } else {
-                  uniqueValueAndCount[ingredient] = 1;
-                }
-              });
-
-              console.warn(uniqueValueAndCount);
-              const ingredientsUsed = Object.keys(uniqueValueAndCount);
-              console.warn('test');
-              const uniqueNameAndCount = {};
-              // get all the raw ingredients
-              ingredData.getIngredients()
-                .then((ingredients) => {
-                  // filter the raw ingredients based on the ingredients used in the orders
-                  ingredientsUsed.forEach((ingredient) => {
-                    const filteredIngredients = ingredients.filter((m) => m.id === ingredient);
-                    // insert the items in the uniqueNameAndCount object
-                    uniqueNameAndCount[filteredIngredients[0].name] = uniqueValueAndCount[ingredient];
-                  });
-                  console.warn(uniqueNameAndCount);
-                  // prints {Beef: 25, Potato: 15, Spinach: 10, Rice: 5, Chicken: 5, ...}
-                  resolve(uniqueNameAndCount);
-                });
+          getIngredsByfilteredOrder(filterdOrdersList)
+            .then((uniqueNameAndCount) => {
+              resolve(uniqueNameAndCount);
             });
         });
     })
     .catch((err) => reject(err));
 });
 
-export default { get7DayIngredAmount };
+export default { get7DayIngredAmount, getOneDayIngredAmount, getAllDayIngredAmount };
